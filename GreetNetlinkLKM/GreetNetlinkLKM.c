c134f01ff3f16fec48bd688edce39865682917e5
Emmanuel Vera <emm.vera@gmail.com>
2023-06-03 00:15:25 +0000

    Completed GreetNetLinkLKM project

--- GreetNetlinkLKM/GreetNetlinkLKM.c
+++ GreetNetlinkLKM/GreetNetlinkLKM.c
@@ -5,12 +5,65 @@
 
 #include "utils/Netlink.h"
 
-#define NETLINK_TEST_PROTOCOL 31
+static inline void nlmsg_dump (struct nlmsghdr* nlh)
+{
+  printk (KERN_INFO "=== NETLINK MESSAGE HEADER ===");
+  printk (KERN_INFO "Length: %d\n", nlh->nlmsg_len);
+  printk (KERN_INFO "Type: %x\n", nlh->nlmsg_type);
+  printk (KERN_INFO "Flags: %x\n", nlh->nlmsg_flags);
+  printk (KERN_INFO "Seq: %d\n", nlh->nlmsg_seq);
+  printk (KERN_INFO "PID: %d\n", nlh->nlmsg_pid);
+  printk (KERN_INFO "==============================");
+}
 
 static struct sock* nl_sk = NULL;
 
 static void netlink_recv_msg_fn (struct sk_buff* skb_in)
 {
+  struct nlmsghdr *recv, *reply;
+  int uspace_pid;
+  char* uspace_data;
+  int uspace_data_len;
+  char kernel_reply[256];
+  struct sk_buff* skb_out;
+  int res;
+
+  printk (KERN_INFO "%s() invoked", __FUNCTION__);
+
+  recv = (struct nlmsghdr*)(skb_in->data);
+
+  nlmsg_dump (recv);
+
+  uspace_pid = recv->nlmsg_pid;
+
+  printk (KERN_INFO "%s(%d) : port id of the sending user space process = %u\n",
+          __FUNCTION__, __LINE__, uspace_pid);
+
+  uspace_data     = (char*)nlmsg_data (recv);
+  uspace_data_len = skb_in->len;
+
+  printk (KERN_INFO "%s(%d) : msg recvd from user space = %s, skb_in->len = "
+                    "%d, nlh->nlmsg_len = %d\n",
+          __FUNCTION__, __LINE__, uspace_data, uspace_data_len, recv->nlmsg_len);
+
+  if (recv->nlmsg_flags & NLM_F_ACK) {
+    memset (kernel_reply, 0, sizeof (kernel_reply));
+    snprintf (kernel_reply, sizeof (kernel_reply),
+              "Msg from Process %d has been processed by kernel", recv->nlmsg_pid);
+
+    skb_out = nlmsg_new (sizeof (kernel_reply), 0);
+    reply =
+    nlmsg_put (skb_out, 0, recv->nlmsg_seq, NLMSG_DONE, sizeof (kernel_reply), 0);
+
+    strncpy (nlmsg_data (reply), kernel_reply, sizeof (kernel_reply));
+
+    res = nlmsg_unicast (nl_sk, skb_out, uspace_pid);
+
+    if (res < 0) {
+      printk (KERN_INFO "Error while sending the data back to user space\n");
+      kfree_skb (skb_out);
+    }
+  }
 }
 
 static struct netlink_kernel_cfg cfg = {
@@ -43,4 +96,6 @@ static void __exit GreetNetlinkLKM_exit (void)
 module_init (GreetNetlinkLKM_init);
 module_exit (GreetNetlinkLKM_exit);
 
+MODULE_AUTHOR ("Emmanuel Vera");
+MODULE_DESCRIPTION ("Greeter example of the netlink sockets");
 MODULE_LICENSE ("GPL");
---
+++ GreetNetlinkLKM/UserSpace.c
@@ -0,0 +1,190 @@
+#include <linux/netlink.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+#include <errno.h>
+#include <memory.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include "utils/Netlink.h"
+
+#define MAX_PAYLOAD 1024
+
+int send_netlink_msg_to_kernel (int sock_fd, char* msg, uint32_t msg_size, int nlmsg_type, uint16_t flags);
+
+int create_netlink_socket (int protocol_number)
+{
+
+  /* Create a net link socket using usual socket() system call
+   * When SOCK_RAW is used, Application has to pepare the struct msghdr
+   * structure and send msg using sendmsg().
+   * When SOCK_DGRAM is used, socket layer will take care to prepare struct
+   * msghdr for you. You have to use sendto() in this case.
+   * In this file, I have demonstrated SOCK_RAW case
+   * */
+
+  int sock_fd = socket (PF_NETLINK, SOCK_RAW, protocol_number);
+
+  return sock_fd;
+}
+
+static void greet_kernel (int sock_fd, char* msg, uint32_t msg_len)
+{
+  send_netlink_msg_to_kernel (sock_fd, msg, msg_len, NLMSG_GREET, NLM_F_ACK);
+}
+
+static void exit_userspace (int sock_fd)
+{
+  close (sock_fd);
+  exit (0);
+}
+
+typedef struct thread_arg_ {
+  int sock_fd;
+} thread_arg_t;
+
+static void* _start_kernel_data_receiver_thread (void* arg)
+{
+  int rc = 0;
+  struct iovec iov;
+  struct nlmsghdr* recv = NULL;
+  static struct msghdr outermsghdr;
+  int sock_fd = 0;
+
+  thread_arg_t* thread_arg = (thread_arg_t*)arg;
+  sock_fd                  = thread_arg->sock_fd;
+
+  recv = (struct nlmsghdr*)calloc (1, NLMSG_HDRLEN + NLMSG_SPACE (MAX_PAYLOAD));
+
+  do {
+    memset (recv, 0, NLMSG_HDRLEN + NLMSG_SPACE (MAX_PAYLOAD));
+
+    iov.iov_base = (void*)recv;
+    iov.iov_len  = NLMSG_HDRLEN + NLMSG_SPACE (MAX_PAYLOAD);
+
+    memset (&outermsghdr, 0, sizeof (struct msghdr));
+
+    outermsghdr.msg_iov     = &iov;
+    outermsghdr.msg_name    = NULL;
+    outermsghdr.msg_iovlen  = 1;
+    outermsghdr.msg_namelen = 0;
+
+    rc = recvmsg (sock_fd, &outermsghdr, 0);
+
+
+    recv          = outermsghdr.msg_iov->iov_base;
+    char* payload = NLMSG_DATA (recv);
+
+    printf ("Received Netlink msg from kernel, bytes recvd = %d\n", rc);
+    printf ("msg recvd from kernel = %s\n", payload);
+  } while (1);
+}
+
+void start_kernel_data_receiver_thread (thread_arg_t* thread_arg)
+{
+  pthread_attr_t attr;
+  pthread_t recv_pkt_thread;
+
+  pthread_attr_init (&attr);
+  pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED);
+
+  pthread_create (&recv_pkt_thread, &attr, _start_kernel_data_receiver_thread,
+                  (void*)thread_arg);
+}
+
+int main (int argc, char** argv)
+{
+  int choice;
+  int sock_fd;
+
+  sock_fd = create_netlink_socket (NETLINK_TEST_PROTOCOL);
+
+  if (sock_fd == -1) {
+    printf ("Error : Netlink socket creation failed : error = %d\n", errno);
+    exit (EXIT_FAILURE);
+  }
+
+  struct sockaddr_nl src_addr;
+
+  struct nlmsghdr* nlh = NULL;
+
+  memset (&src_addr, 0, sizeof (src_addr));
+
+  src_addr.nl_family = AF_NETLINK;
+  src_addr.nl_pid    = getpid ();
+
+  if (bind (sock_fd, (struct sockaddr*)&src_addr, sizeof (src_addr)) == -1) {
+    printf ("Error : bind has failed\n");
+    exit (1);
+  }
+
+  thread_arg_t thread_arg;
+  thread_arg.sock_fd = sock_fd;
+
+  start_kernel_data_receiver_thread (&thread_arg);
+
+  while (1) {
+    printf ("Main-Menu\n");
+    printf ("\t1. Greet Kernel\n");
+    printf ("\t2. Exit\n");
+    printf ("choice ? ");
+    scanf ("%d", &choice);
+    getc (stdin);
+
+    switch (choice) {
+    case 1: {
+      char user_msg[MAX_PAYLOAD];
+      memset (user_msg, 0, MAX_PAYLOAD);
+      if (fgets ((char*)user_msg, MAX_PAYLOAD - 1, stdin) == NULL) {
+        printf ("error in reading from stdin\n");
+        exit (EXIT_FAILURE);
+      }
+      greet_kernel (sock_fd, user_msg, sizeof (user_msg));
+    } break;
+    case 2: exit_userspace (sock_fd); break;
+    default:;
+    }
+  }
+  return 0;
+}
+
+int send_netlink_msg_to_kernel (int sock_fd, char* msg, uint32_t msg_size, int nlmsg_type, uint16_t flags)
+{
+  struct sockaddr_nl dest_addr;
+  memset (&dest_addr, 0, sizeof (dest_addr));
+  dest_addr.nl_family = AF_NETLINK;
+  dest_addr.nl_pid    = 0;
+
+  struct nlmsghdr* nlh =
+  (struct nlmsghdr*)calloc (1, NLMSG_HDRLEN + NLMSG_SPACE (msg_size));
+
+  nlh->nlmsg_len   = NLMSG_HDRLEN + NLMSG_SPACE (msg_size);
+  nlh->nlmsg_pid   = getpid ();
+  nlh->nlmsg_flags = flags;
+  nlh->nlmsg_type  = nlmsg_type;
+  nlh->nlmsg_seq   = 0;
+
+  strncpy (NLMSG_DATA (nlh), msg, msg_size);
+
+  struct iovec iov;
+  iov.iov_base = (void*)nlh;
+  iov.iov_len  = nlh->nlmsg_len;
+
+  static struct msghdr outermsghdr;
+
+  memset (&outermsghdr, 0, sizeof (struct msghdr));
+  outermsghdr.msg_name    = (void*)&dest_addr;
+  outermsghdr.msg_namelen = sizeof (dest_addr);
+  outermsghdr.msg_iov     = &iov;
+  outermsghdr.msg_iovlen  = 1;
+
+  int rc = sendmsg (sock_fd, &outermsghdr, 0);
+  if (rc < 0) {
+    printf ("Msg Sending Failed, error no = %d\n", errno);
+  }
+
+  return rc;
+}
